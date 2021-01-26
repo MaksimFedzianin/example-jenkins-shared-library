9220bd973caeda5dff7bc0b8dd7405cbfbc72983
Maksim Fedzianin <maksim.fedzianin@gmail.com>
2021-01-26 22:49:53 +0300

    Added body to jobGenerator

--- vars/jobGenerator.groovy
+++ vars/jobGenerator.groovy
@@ -1,4 +1,19 @@
-def call() {
+def call(body) {
+	
+	def inputParams = [
+		metaRelativePath     : 'jenkins/meta.groovy',
+		gitCredentialsId     : 'jenkins-git',
+		devopsGitUrl         : null,
+		devopsGitBranch      : 'dev',
+		ansibleInventoryName : null
+	]
+
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = inputParams
+	body()
+	
+	echo 'devopsGitUrl is :' + inputParams.devopsGitUrl
+	
     node {
         //checkout scm
         stage('Install') {
