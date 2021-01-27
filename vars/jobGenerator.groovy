b08775faaaba3e94c4a1580842f43bbdcb39acd5
Maksim Fedzianin <maksim.fedzianin@gmail.com>
2021-01-27 18:30:34 +0300

    Added branch parsing

--- vars/jobGenerator.groovy
+++ vars/jobGenerator.groovy
@@ -13,10 +13,9 @@ def call(body) {
 	checkout scm: [$class: 'GitSCM',
 									   userRemoteConfigs: [[url: inputParams.repoUrl]]
 						]
-	remotes = bat(returnStdout: true, script: 'git branch -a | findstr \"remotes/origin\"')
-	echo remotes
-	remotes = remotes.replace("remotes/origin/", "")
-	echo 'branches without prefix is: ' + remotes
+	
+	branches = getRepositoryBranches()
+	echo 'parsed branches are: ' + branches
     node {
         //checkout scm
         stage('Install') {
@@ -29,4 +28,14 @@ def call(body) {
             echo 'Deploying...'
         }
     }
+}
+
+private List getRepositoryBranches(){
+	remotes = bat(returnStdout: true, script: 'git branch -a | findstr \"remotes/origin\"')
+	echo remotes
+	remotes = remotes.replace("remotes/origin/", "")
+	remotes = remotes.replace("/", "_")
+	echo 'branches without prefix is: ' + remotes
+	
+	return remotes.stdout.readLines().drop(1)
 }
