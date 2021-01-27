5b467616939a73276dcc82085def2f771851e9e3
Maksim Fedzianin <maksim.fedzianin@gmail.com>
2021-01-27 18:39:01 +0300

    Fixed error

--- vars/jobGenerator.groovy
+++ vars/jobGenerator.groovy
@@ -37,5 +37,7 @@ private List getRepositoryBranches(){
 	remotes = remotes.replace("/", "_")
 	echo 'branches without prefix is: ' + remotes
 	
-	return remotes.readLines().drop(1)
+	List result  = remotes.readLines().drop(1)
+	echo 'result size is: ' + result.size()  
+	return result
 }
