a30b1bb381fb9041bec4da886b67082acc79fa84
Maksim Fedzianin <maksim.fedzianin@gmail.com>
2021-01-31 17:23:55 +0300

    Added GitHelper

--- vars/jobGenerator.groovy
+++ vars/jobGenerator.groovy
@@ -7,7 +7,7 @@ def call(body) {
 	body()
 	
 	echo 'repoUrl is :' + inputParams.repoUrl
-	echo new GitHelper().getBranches(inputParams.repoUrl).toString()
+	echo new GitHelper(this).getBranches(inputParams.repoUrl).toString()
 	
     node {
         stage('Pull source') {
